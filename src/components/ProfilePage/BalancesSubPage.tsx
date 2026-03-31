44727b3341d28a53ac3f80bc0c2a3c2e278cf0b1
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-03-30 21:59:19 -0300

    add chart package

--- src/components/ProfilePage/BalancesSubPage.tsx
+++ src/components/ProfilePage/BalancesSubPage.tsx
@@ -1,11 +1,13 @@
 "use client";
 
+import { Balances } from "../Balances/Balances";
+import { BalancesChart } from "./BalancesChart";
+
 export function BalancesSubPage() {
   return (
-    <div className="flex flex-col items-center gap-6 pt-8">
-      <p className="text-main-darkPurple text-base font-normal text-center leading-6">
-        Balances coming soon.
-      </p>
+    <div className="flex flex-col gap-4">
+      <Balances />
+      <BalancesChart />
     </div>
   );
 }
