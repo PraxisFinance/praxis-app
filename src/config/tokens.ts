833f3221ecf5ea66d406dd462553043b364ef67f
aii23 <a.ivlev.om@gmail.com>
2026-04-08 10:05:21 +0300

    New token addresses

--- src/config/tokens.ts
+++ src/config/tokens.ts
@@ -1,7 +1,7 @@
 export const TOKEN_ADDRESSES = {
-  USDC: "0x6a10a9fd32b894A1d4E10E957A73160067B047b6" as `0x${string}`,
-  PT: "0xA00510EcC1600133972C7Bbd93f7a002f78E90BE" as `0x${string}`,
-  YT: "0x30EE6bB8BdC76ac9ff99eD30bf06E663221DE7b7" as `0x${string}`,
+  USDC: "0x8fd548931F206cbB89e44a9b33e87B59Fced7669" as `0x${string}`,
+  PT: "0x89C52B22BA0928c4AD874d57Fa3b0EC840Dc0Ac7" as `0x${string}`,
+  YT: "0x2dA96049a126abe06258ba168bF525A54B6e9732" as `0x${string}`,
 } as const;
 
 export const TOKEN_DECIMALS = {
