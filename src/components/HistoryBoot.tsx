ea75070bf0fa2b84bea93821748ea1183df53522
aii23 <a.ivlev.om@gmail.com>
2026-05-23 13:33:51 +0300

    Fixed mock

--- src/components/HistoryBoot.tsx
+++ src/components/HistoryBoot.tsx
@@ -4,8 +4,7 @@ import { useAccount } from "wagmi";
 import { useUserHistory } from "@/hooks/useUserHistory";
 
 export function HistoryBoot() {
-  // const { address } = useAccount();
-  const address = "0x75a9D238a0c924db5f25258C9636007866a1Beae";
+  const { address } = useAccount();
   useUserHistory(address);
   return null;
 }
