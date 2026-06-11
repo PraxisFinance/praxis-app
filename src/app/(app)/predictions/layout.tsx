0a58b5f5701839153e48be2f46a8e73a75aa84a9
Ivan Kireev <31880548+sm4chnik@users.noreply.github.com>
2026-06-10 21:53:44 -0300

    fix files by new routes

--- src/app/(app)/predictions/layout.tsx
+++ src/app/(app)/predictions/layout.tsx
@@ -1,15 +1,10 @@
 import { Balances } from "@/components/Balances/Balances";
 import { PredictionsHubLayoutShell } from "@/components/PredictionsPage/PredictionsHubLayoutShell";
-import { PredictionsTabBarHost } from "@/components/PredictionsPage/PredictionsTabBarHost";
 
 export default function PredictionsLayout({ children }: { children: React.ReactNode }) {
   return (
     <PredictionsHubLayoutShell>
-      <div className="flex flex-col gap-6">
-        {/* <Balances /> */}
-        <PredictionsTabBarHost />
-        {children}
-      </div>
+      <div className="flex flex-col gap-6">{children}</div>
     </PredictionsHubLayoutShell>
   );
 }
