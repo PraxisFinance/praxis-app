7dc0617cd32fcb0ef123d27793781a64bd4919ce
aii23 <a.ivlev.om@gmail.com>
2026-06-19 04:53:12 +0300

    Added ryd metadata pull for ryd store

--- src/lib/trpc/routers/_app.ts
+++ src/lib/trpc/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from "../init";
 import { offchainEventsRouter } from "./offchainEvents";
+import { rydContractsRouter } from "./rydContracts";
 import { twoPoolContractsRouter } from "./twoPoolContracts";
 import { userHistoryRouter } from "./userHistory";
 
 export const appRouter = router({
   offchainEvents: offchainEventsRouter,
+  rydContracts: rydContractsRouter,
   twoPoolContracts: twoPoolContractsRouter,
   userHistory: userHistoryRouter,
 });
